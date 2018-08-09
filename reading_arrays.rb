5e0873ace423c2c69d2f51e58b20238384d7f03b
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 13:52:45 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,32 +1,34 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
+STUDENT_NAMES = ["Adele", "Beyonce", "Cardi_B", "Lady_Gaga", "Nicki_Minaj", "Rihanna"]
 
-def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
+# puts STUDENT_NAMES[0]
+def first_name
+  # puts "First Name"
+  puts STUDENT_NAMES[0]
+end 
+first_name
 
-def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+def fourth_name
+  # puts "Fourth Name"
+  puts STUDENT_NAMES[3]
 end
+fourth_name
 
-def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+puts STUDENT_NAMES.last
+def Last_Student
+  puts " Last Name"
+  puts STUDENT_NAMES.last 
 end
+ Last_Student()
 
-def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
-end
 
-def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
-end
+# def first_student_by_method
+#   # Write a solution that returns the first student using the built in .first method
+# end
 
-def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
-end
+# def last_student_by_method
+#   # Write a solution that returns the first student using the built in .last method
+# end
+
+# def first_second_and_third_students
+#   # Write a solution that returns the first, second and third students
+# end
